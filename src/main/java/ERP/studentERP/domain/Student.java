33f7c6704813286cb54530b22b0a627c75f7bedb
Aryan Mishra <aryanmishracoc@gmail.com>
2022-12-13 15:29:13 +0530

    index and result added

--- src/main/java/ERP/studentERP/domain/Student.java
+++ src/main/java/ERP/studentERP/domain/Student.java
@@ -5,5 +5,21 @@ public class Student {
 	public Student() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	private String fname;
+	private String lname;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
 
 }
